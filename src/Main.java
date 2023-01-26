617016a2c06d153215fcde1c84dc8733586229bc
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-01-26 16:28:53 +0300

    task 1-3 (with method static and void)

--- src/Main.java
+++ src/Main.java
@@ -8,55 +8,68 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         //пишем код для задачи 1
-        int year = 2023;
-        checkYear(year);
+        int year = 2024;
+        int print = checkYear(year);
+        printYear(year,print);
     }
-
-    public static void checkYear(int year) {
+    public static int checkYear(int year) {
         if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    public static void printYear(int year, int print) {
+        if (print == 0) {
             System.out.println("год " + year + " является високосным");
         } else {
             System.out.println("год " + year + " не является високосным");
         }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
         //пишем код для задачи 2
         int clientOS = 1;
         LocalDate currentDate = LocalDate.now();
         int currentYear = currentDate.getYear();
-        getClient(clientOS,currentYear);
+        int os = getClient(clientOS,currentYear);
+        printClient(os);
     }
-    public static void getClient(int clientOS, int currentYear) {
+    public static int getClient(int clientOS, int currentYear) {
         if (clientOS == 0) {
             if (currentYear > 2015) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
+                return 0;
             }else {
-                System.out.println("Установите облегченную версию для iOS по ссылке");
+                return 1;
             }
         } else {
             if (currentYear < 2015) {
-                System.out.println("Установите облегченную версию для Android по ссылке");
+                return 2;
             } else {
-                System.out.println("Установите версию для Android по ссылке");
+                return 3;
             }
+        }
+    }
+    public static void printClient(int os) {
+        if (os == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os == 1) {
+            System.out.println("Установите облегченную версию для iOS по ссылке");
+        } else if (os == 2) {
+            System.out.println("Установите облегченную версию для Android по ссылке");
+        } else {
+            System.out.println("Установите версию для Android по ссылке");
         }
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         //пишем код для задачи 3
-        int deliveryDistance = 70;
-        int time = delivery(deliveryDistance);
-        if (delivery(deliveryDistance) == 0) {
-            System.out.println("Доставки нет");
-        } else {
-            System.out.println("Потребуется дней: " + time);
-        }
+        int deliveryDistance = 110;
+        int time = timingDelivery(deliveryDistance);
+        printResult(time);
     }
-
-    public static int delivery(int distance) {
+    public static int timingDelivery (int distance) {
         int delivery = 0;
         if (distance <= 20) {
             delivery++;
@@ -69,6 +82,13 @@ public class Main {
             return delivery;
         } else {
             return 0;
+        }
+    }
+    public static void printResult (int time) {
+        if (time == 0) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + time);
         }
     }
 }
